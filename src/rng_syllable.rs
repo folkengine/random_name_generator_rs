64a2a88a6b381a29aaff8469a632fb41c9167d6f
Christoph <gaoler@electronicpanopticon.com>
2020-07-27 20:27:50 -0400

    Starting on syllable

--- src/rng_syllable.rs
+++ src/rng_syllable.rs
@@ -1,22 +1,47 @@
-enum Classification {
+use regex::Regex;
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Classification {
     Prefix,
     Center,
     Suffix,
 }
 
-enum Rule {
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Rule {
     Consonant,
     Vowel,
     None,
 }
 
-struct Syllable {
+pub struct Syllable {
     value: String,
     classification: Classification,
     rule: Rule,
 }
 
+impl Syllable {
+    // pub fn new(raw: String) -> Syllable {
+    //     Syllable
+    // }
+
+    fn classify(raw: String) -> (Classification, String) {
+        return (Classification::Prefix, raw);
+    }
+
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    #[test]
+    fn classify_prefix() {
+        let (classification, s) = Syllable::classify("-ansr +v".to_string());
+
+        assert_eq!(Classification::Prefix, classification);
+        // assert_eq!("ansr +v".to_string(), s);
+    }
 }
