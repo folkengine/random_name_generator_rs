8dce3a88d1aa1f82f0183adf533ea6c6370504a2
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2023-02-11 16:31:57 -0800

    getting there

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,143 @@
-use clap::{crate_authors, crate_name, crate_version, Command, ArgMatches};
+use clap::{command, Arg, ArgAction, ArgMatches};
 use rnglib::{Language, RNG};
 
-const PKG_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+// ArgMatches {
+//     valid_args: ["elven", "fantasy", "goblin", "roman", "curse", "demonic", "dump", "flipmode", "raw", "help", "version"],
+//     valid_subcommands: [],
+//     args: FlatMap { keys: ["fantasy", "elven", "goblin", "roman", "curse", "demonic", "dump", "flipmode"],
+//     values: [MatchedArg { source: Some(CommandLine), indices: [1], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["true"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [2], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [3], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [4], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [5], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [6], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [7], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [8], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }] }, subcommand: None }
+//
+// ArgMatches {
+//     valid_args: ["elven", "fantasy", "goblin", "roman", "curse", "demonic", "dump", "flipmode", "raw", "help", "version"],
+//     valid_subcommands: [],
+//     args: FlatMap { keys: ["elven", "fantasy", "goblin", "roman", "curse", "demonic", "dump", "flipmode"],
+//     values: [MatchedArg { source: Some(CommandLine), indices: [1], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["true"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [2], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [3], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [4], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [5], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [6], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [7], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }, MatchedArg { source: Some(DefaultValue), indices: [8], type_id: Some(bool), vals: [[AnyValue { inner: bool }]], raw_vals: [["false"]], ignore_case: false }] }, subcommand: None }
 
 fn main() {
-    let matches = get_matches();
+    let matches = cmd().get_matches();
 
     let rng = determine_language(&matches);
 
-    if matches.is_present("dump") {
-        dump(rng)
-    } else if matches.is_present("curse") {
-        curse(rng)
-    } else {
-        generate_name(rng)
-    }
+    // println!("{:?}", matches);
+
+    // generate_name(rng);
+
+    generate_name(rng)
 }
 
-fn dump(rng: rnglib::RNG) {
-    for s in rng.syllables().into_iter() {
-        println!("{}", s)
-    }
+fn cmd() -> clap::Command {
+    command!()
+        .arg(
+            Arg::new("elven")
+                .short('e')
+                .long("elven")
+                .required(false)
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("fantasy")
+                .short('f')
+                .long("fantasy")
+                .required(false)
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("goblin")
+                .short('g')
+                .long("goblin")
+                .required(false)
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("roman")
+                .short('r')
+                .long("roman")
+                .required(false)
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("curse")
+                .short('c')
+                .long("curse")
+                .required(false)
+                .action(ArgAction::SetTrue)
+                .help("[UNDER CONSTRUCTION]"),
+        )
+        .arg(
+            Arg::new("demonic")
+                .short('d')
+                .long("demonic")
+                .required(false)
+                .action(ArgAction::SetTrue)
+                .help("[UNDER CONSTRUCTION]"),
+        )
+        .arg(
+            Arg::new("flipmode")
+                .short('x')
+                .long("flipmode")
+                .required(false)
+                .action(ArgAction::SetTrue)
+                .help("Flipmode is the greatest! (Random language)"),
+        )
+        .arg(
+            Arg::new("raw")
+                .long("raw")
+                .required(false)
+                .value_name("FILE")
+                .help("Reads in a raw language file"),
+        )
 }
+
+fn determine_language(matches: &ArgMatches) -> RNG {
+    if matches.get_flag("elven") {
+        RNG::new(&Language::Elven).unwrap()
+    } else if matches.get_flag("fantasy") {
+        RNG::new(&Language::Fantasy).unwrap()
+    } else if matches.get_flag("goblin") {
+        RNG::new(&Language::Goblin).unwrap()
+    } else if matches.get_flag("roman") {
+        RNG::new(&Language::Roman).unwrap()
+    } else if matches.get_flag("curse") {
+        RNG::new(&Language::Curse).unwrap()
+    } else if matches.get_flag("demonic") {
+        RNG::new(&Language::Demonic).unwrap()
+    } else {
+        let my_dialect_type: Language = rand::random();
+        RNG::new(&my_dialect_type).unwrap()
+    }
+    // match matches {
+    //     &_ => {
+    //         RNG::new(&Language::Elven).unwrap()
+    //     }
+    // }
+    // if matches.contains_id("elven") {
+    //     RNG::new(&Language::Elven).unwrap()
+    // } else if matches.contains_id("fantasy") {
+    //     RNG::new(&Language::Fantasy).unwrap()
+    // } else {
+    //     let my_dialect_type: Language = rand::random();
+    //     RNG::new(&my_dialect_type).unwrap()
+    // }
+    // if matches.get_flag("e") {
+    //     RNG::new_from_file(matches.value_of("raw").unwrap().to_string()).unwrap()
+    // } else if matches.is_present("elven") {
+    //     RNG::new(&Language::Elven).unwrap()
+    // } else if matches.is_present("fantasy") {
+    //     RNG::new(&Language::Fantasy).unwrap()
+    // } else if matches.is_present("goblin") {
+    //     RNG::new(&Language::Goblin).unwrap()
+    // } else if matches.is_present("roman") {
+    //     RNG::new(&Language::Roman).unwrap()
+    // } else if matches.is_present("demonic") {
+    //     RNG::new(&Language::Demonic).unwrap_err()
+    // } else if matches.is_present("curse") {
+    //     RNG::new(&Language::Curse).unwrap()
+    // } else {
+    //     let my_dialect_type: Language = rand::random();
+    //     RNG::new(&my_dialect_type).unwrap()
+    // }
+}
+
 
 fn curse(rng: rnglib::RNG) {
     let word = rng.generate_short();
@@ -35,40 +151,22 @@ fn generate_name(rng: rnglib::RNG) {
     println!("{}: {} {}", rng.name, first_name, last_name)
 }
 
-fn determine_language(matches: &ArgMatches) -> RNG {
-    if matches.is_present("raw") {
-        RNG::new_from_file(matches.value_of("raw").unwrap().to_string()).unwrap()
-    } else if matches.is_present("elven") {
-        RNG::new(&Language::Elven).unwrap()
-    } else if matches.is_present("fantasy") {
-        RNG::new(&Language::Fantasy).unwrap()
-    } else if matches.is_present("goblin") {
-        RNG::new(&Language::Goblin).unwrap()
-    } else if matches.is_present("roman") {
-        RNG::new(&Language::Roman).unwrap()
-    } else if matches.is_present("demonic") {
-        RNG::new(&Language::Demonic).unwrap_err()
-    } else if matches.is_present("curse") {
-        RNG::new(&Language::Curse).unwrap()
-    } else {
-        let my_dialect_type: Language = rand::random();
-        RNG::new(&my_dialect_type).unwrap()
-    }
-}
+//
+// fn get_matches() -> ArgMatches {
+//     command!()
+//         .arg("-c, --curse 'Use the Curse language [UNDER CONSTRUCTION]'")
+//         .arg("-d, --demonic 'Use the Demonic language [UNDER CONSTRUCTION]'")
+//         .arg("-e, --elven 'Use the Elven language'")
+//         .arg("-f, --fantasy 'Use the Fantasy language'")
+//         .arg("-g, --goblin 'Use the Goblin language'")
+//         .arg("-r, --roman 'Use the Roman language'")
+//         .arg("--dump 'Print out the raw lanuage file'")
+//         .arg("-x, --flipmode 'Use a random language'")
+//         .arg("--raw=[FILE] 'reads in a raw language file'")
+//         .get_matches()
+// }
 
-fn get_matches() -> ArgMatches {
-    Command::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(PKG_DESCRIPTION)
-        .arg("-c, --curse 'Use the Curse language [UNDER CONSTRUCTION]'")
-        .arg("-d, --demonic 'Use the Demonic language [UNDER CONSTRUCTION]'")
-        .arg("-e, --elven 'Use the Elven language'")
-        .arg("-f, --fantasy 'Use the Fantasy language'")
-        .arg("-g, --goblin 'Use the Goblin language'")
-        .arg("-r, --roman 'Use the Roman language'")
-        .arg("--dump 'Print out the raw lanuage file'")
-        .arg("-x, --flipmode 'Use a random language'")
-        .arg("--raw=[FILE] 'reads in a raw language file'")
-        .get_matches()
+#[test]
+fn verify_cmd() {
+    cmd().debug_assert();
 }
