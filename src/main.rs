c257366a890da9dd5c29ba9e7a1918cf64cce36b
folkengine <gaoler@electronicpanopticon.com>
2023-02-23 19:03:48 -0800

    Added RNG::random()

--- src/main.rs
+++ src/main.rs
@@ -112,7 +112,7 @@ fn get_rng(matches: &ArgMatches) -> Result<RNG, RNGError> {
     let is_russian = matches.get_flag("russian");
 
     if matches.get_flag("demonic") {
-        Ok(RNG::try_from(&Language::Demonic)?)
+        Ok(RNG::from(&Language::Demonic))
     } else if matches.get_flag("elven") {
         filter_russian(is_russian, &Language::Elven, &Language::Эльфийский)
     } else if matches.get_flag("fantasy") {
@@ -122,10 +122,9 @@ fn get_rng(matches: &ArgMatches) -> Result<RNG, RNGError> {
     } else if matches.get_flag("roman") {
         filter_russian(is_russian, &Language::Roman, &Language::Римский)
     } else if matches.get_flag("curse") {
-        Ok(RNG::try_from(&Language::Curse)?)
+        Ok(RNG::from(&Language::Curse))
     } else if matches.get_flag("flipmode") {
-        let my_dialect_type: Language = rand::random();
-        Ok(RNG::try_from(&my_dialect_type)?)
+        Ok(RNG::random())
     } else {
         let raw = matches.get_one::<String>("raw").unwrap();
         let result = RNG::new_from_file(raw.clone());
@@ -137,15 +136,11 @@ fn get_rng(matches: &ArgMatches) -> Result<RNG, RNGError> {
     }
 }
 
-fn filter_russian(
-    is_russian: bool,
-    english: &Language,
-    russian: &Language,
-) -> Result<RNG, RNGError> {
+fn filter_russian(is_russian: bool, english: &Language, russian: &Language) -> Result<RNG, RNGError> {
     if is_russian {
-        Ok(RNG::try_from(russian)?)
+        Ok(RNG::from(russian))
     } else {
-        Ok(RNG::try_from(english)?)
+        Ok(RNG::from(english))
     }
 }
 
